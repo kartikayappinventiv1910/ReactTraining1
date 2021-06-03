d92177745b1840486480ad3d8febedd1ae189e56
kartikayappinventiv1910 <kartikay.sharma@appinventiv.com>
2021-06-03 10:40:03 +0530

    complete assignment1 index.js

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -24,20 +24,20 @@ class Home extends Component {
           <div className={"itemContainer2"}>
             <div className={"itemcont2Head"}>
               <p className={"container2Head2"}>
-                Start now<br></br> your <b>free plan.</b>
+                {"Start now"}<br></br> {"your"} <b>{"free plan."}</b>
               </p>
             </div>
             <div className={"content2"}>
               <div className={"container2ContentHead"}>
-                <p className={"contentHead1"}>Lite</p>
-                <p className={"contentHead2"}>Free</p>
+                <p className={"contentHead1"}>{"Lite"}</p>
+                <p className={"contentHead2"}>{"Free"}</p>
               </div>
               <div className={"container2ContentHead"}>
                 <hr className={"horizontal1"} />
-                <p className={"contentHead3"}>with restrictions</p>
+                <p className={"contentHead3"}>{"with restrictions"}</p>
               </div>
               <p className={"contentTopic"}>
-                <b>Plan includes:</b>
+                <b>{"Plan includes:"}</b>
               </p>
               <div className={"content2Points"}>
                 <div className={"longpara"}>
@@ -46,55 +46,53 @@ class Home extends Component {
                   </div>
                   <div className={"longparacont"}></div>
                   <p className={"longpoints"}>
-                    Manage conversations directly from your website.
+                    {"Manage conversations directly from your website."}
                   </p>
                 </div>
               </div>
               <div className={"pointsblock"}>
                 <p className={"points"}>
-                  <span className={"tick1"}>&#10003;</span>Bot without the AI
-                  service.
+                  <span className={"tick1"}>&#10003;</span>{"Bot without the AI service."}
                 </p>
               </div>
               <div className={"pointsblock"}>
                 <p className={"points"}>
-                  <span className={"tick1"}>&#10003;</span>Achieved chat for 30
-                  days.
+                  <span className={"tick1"}>&#10003;</span>{"Achieved chat for 30 days."}
                 </p>
               </div>
               <div className={"pointsblock"}>
                 <p className={"points"}>
-                  <span className={"tick1"}>&#10003;</span>Free,for always.
+                  <span className={"tick1"}>&#10003;</span>{"Free,for always."}
                 </p>
               </div>
               <div className={"viewFeature2"}>
-                <a href={"#"}>View all the features</a>
+                <a href={"#"}>{"View all the features"}</a>
               </div>
               <button className={"button2"}>
-                Start 14 days of free Pro plan
+                {"Start 14 days of free Pro plan"}
               </button>
             </div>
           </div>
           <div className={"itemContainer3"}>
             <div className={"itemcont3Head"}>
               <p className={"container3Head"}>
-                Are you a corporate?<br></br>
+                {"Are you a corporate?"}<br></br>
                 <a href={"#"} className={"contactus"}>
-                  Contact us
+                  {"Contact us"}
                 </a>
               </p>
             </div>
             <div className={"content3"}>
               <div className={"container3ContentHead"}>
-                <p className={"content3Head1"}>Pro</p>
-                <p className={"content3Head2"}>29$</p>
+                <p className={"content3Head1"}>{"Pro"}</p>
+                <p className={"content3Head2"}>{"29$"}</p>
               </div>
               <div className={"container3ContentHead"}>
                 <hr className={"horizontal2"} />
-                <p className={"content3Head3"}>montly</p>
+                <p className={"content3Head3"}>{"montly"}</p>
               </div>
               <p className={"content2Topic"}>
-                <b>All of Lite plus:</b>
+                <b>{"All of Lite plus:"}</b>
               </p>
               <div className={"content2Points"}>
                 <div className={"longpara"}>
@@ -103,15 +101,13 @@ class Home extends Component {
                   </div>
                   <div className={"longparacont"}></div>
                   <p className={"longpoints3"}>
-                    Bot with AI that can recognize the user's behaviour and can
-                    automatize the sentences.
+                    {"Bot with AI that can recognize the user's behaviour and can automatize the sentences."}
                   </p>
                 </div>
               </div>
               <div className={"pointsblock3"}>
                 <p className={"points3"}>
-                  <span className={"tick2"}>&#10003;</span>Unlimited
-                  conversational flows.
+                  <span className={"tick2"}>&#10003;</span>{"Unlimited conversational flows."}
                 </p>
               </div>
               <div className={"content2Points"}>
@@ -121,21 +117,20 @@ class Home extends Component {
                   </div>
                   <div className={"longparacont"}></div>
                   <p className={"longpoints3"}>
-                    Facebook, Twitter, Instagram and Linkedin integration.
+                    {"Facebook, Twitter, Instagram and Linkedin integration."}
                   </p>
                 </div>
               </div>
               <div className={"pointsblock3"}>
                 <p className={"points3"}>
-                  <span className={"tick2"}>&#10003;</span>Achieved chat without
-                  limits.
+                  <span className={"tick2"}>&#10003;</span>{"Achieved chat without limits."}
                 </p>
               </div>
               <div className={"viewFeature3"}>
-                <a href={"#"}>View all the features</a>
+                <a href={"#"}>{"View all the features"}</a>
               </div>
               <button className={"button3"}>
-                Start 14 days of free Pro plan
+                {"Start 14 days of free Pro plan"}
               </button>
             </div>
           </div>
